be5b2b4a799c46d6c7e9c3c9055f4f62886cc3c1
Sascha Ende <sascha@sascha-ende.de>
2019-04-25 09:39:09 +0200

    - Enhanced hosts detection - New spam host

--- Classes/Domain/Repository/BannedHostsRepository.php
+++ Classes/Domain/Repository/BannedHostsRepository.php
@@ -12,7 +12,16 @@ class BannedHostsRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
         /** @var QueryResult $results */
         $results = $this->findByHost($emailparts[1]);
 
-        if ($results->count() >= 1) {
+        // Get the main domain
+        $domainparts = explode('.',$emailparts[1]);
+
+
+        $hostname = $domainparts[count($domainparts)-2].'.'.$domainparts[count($domainparts)-1];
+
+        /** @var QueryResult $results2 */
+        $results2 = $this->findByHost($hostname);
+
+        if ($results->count() >= 1 || $results2->count() >= 1) {
             return true;
         } else {
             return false;
