1cfb573ee9fe24b70345800b684122277985414a
Sascha Ende <sascha@sascha-ende.de>
2019-05-10 21:02:58 +0200

    Modified   ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.5.5',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.9.99',
+            'typo3' => '8.7.0-9.9.99',
             't3helpers' => '0.9.25',
         ],
         'conflicts' => [],
