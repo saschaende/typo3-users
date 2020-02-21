ae6f23faef752ce05854745a339b906a06326da7
Sascha Ende <sascha@sascha-ende.de>
2020-02-21 17:42:28 +0100

    Modified   Classes/Domain/Model/BannedHosts.php Modified   Classes/Domain/Model/BannedMails.php Modified   Classes/Domain/Model/Pages.php Modified   Classes/Domain/Repository/BannedHostsRepository.php Modified   Classes/Domain/Repository/BannedMailsRepository.php Modified   Classes/Domain/Repository/PagesRepository.php Add        Classes/Controller/ProfileController.php Add        Resources/Private/Templates/Profile/Show.html

---
+++ Classes/Controller/ProfileController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SaschaEnde\Users\Controller;
+
+use SaschaEnde\Users\Domain\Model\User;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+class ProfileController extends ActionController {
+
+    public function show(User $user){
+        $this->view->assignMultiple([
+            'user' => $user
+        ]);
+    }
+
+}
